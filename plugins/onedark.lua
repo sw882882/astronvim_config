e3147b5a7eb771cd0c7fa871c14d8db7cd800cc1
Andrew <seungwan.wee@gmail.com>
2023-06-29 13:40:17 +0700

    change theme

---
+++ plugins/onedark.lua
@@ -0,0 +1,9 @@
+return {
+  "navarasu/onedark.nvim",
+  name = "onedark",
+  config = function()
+    require("onedark").setup {
+      style = "cool",
+    }
+  end,
+}
---
+++ plugins/onenord.lua
@@ -0,0 +1,4 @@
+return {
+  "rmehri01/onenord.nvim",
+  config = function() require("onenord").setup() end,
+}
